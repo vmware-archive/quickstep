588791e81a8a8ccb550b515c3f105e5f8f1350fa
marc <marc.spehlmann@gmail.com>
2016-01-31 13:54:29 -0600

    space

--- query_optimizer/resolver/Resolver.cpp
+++ query_optimizer/resolver/Resolver.cpp
@@ -385,7 +385,7 @@ L::LogicalPtr Resolver::resolveCreateTable(
     attribute_name_set.insert(lower_attribute_name);
   }
 
-  if(create_table_statement.block_properties() != nullptr) {
+  if (create_table_statement.block_properties() != nullptr) {
     THROW_SQL_ERROR_AT(create_table_statement.block_properties())
         << "BLOCKPROPERTIES is not supported.";
   }
