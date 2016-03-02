a0d69aaa843b85a24cf9b63abaa258bec45fc931
Hakan Memisoglu <hakanmemisoglu@gmail.com>
2016-03-02 12:44:05 -0600

    LockTable.cpp comments are added.

--- transaction/LockTable.cpp
+++ transaction/LockTable.cpp
@@ -6,11 +6,15 @@ namespace transaction {
 
 LockTableResult
 LockTable::putLock(TransactionId tid, const ResourceId &rid,
-                   AccessMode access_mode) {
+		   AccessMode access_mode) {
   LockListPair &lock_list_pair = internal_map_[rid];
+
+  // Each resource id entry has own list and pending list.
   LockOwnList &lock_own_list = lock_list_pair.first;
   LockPendingList &lock_pending_list = lock_list_pair.second;
 
+  // Check this resource id already has the same lock request from
+  // the same transaction in the own list.
   for (LockOwnList::const_iterator it = lock_own_list.cbegin();
        it != lock_own_list.cend(); ++it) {
     if (it->first == tid && it->second.getAccessMode() == access_mode) {
@@ -18,6 +22,8 @@ LockTable::putLock(TransactionId tid, const ResourceId &rid,
     }
   }
 
+  // Check this resource id already has the same lock request from
+  // the same transaction in the pending list.
   for (LockPendingList::const_iterator it = lock_pending_list.cbegin();
        it != lock_pending_list.cend(); ++it) {
     if (it->first == tid && it->second.getAccessMode() == access_mode) {
@@ -25,6 +31,8 @@ LockTable::putLock(TransactionId tid, const ResourceId &rid,
     }
   }
 
+  // If the execution can reach this point, it means the resource id
+  // does not have duplicate lock record (for both in owned and pending).
   if (lock_pending_list.empty()) {
     for (LockOwnList::const_iterator it = lock_own_list.cbegin();
          it != lock_own_list.cend(); ++it) {
@@ -38,6 +46,9 @@ LockTable::putLock(TransactionId tid, const ResourceId &rid,
     lock_own_list.push_back(std::make_pair(tid, Lock(rid, access_mode)));
     return LockTableResult::kPLACED_IN_OWNED;
   } else {
+    // If the pending list is not empty, even if the lock request is compatible
+    // with other owned lock entries, we put the new request into the pending
+    // list to eliminate starvation.
     lock_pending_list.push_back(std::make_pair(tid, Lock(rid, access_mode)));
     return LockTableResult::kPLACED_IN_PENDING;
   }
@@ -46,27 +57,41 @@ LockTable::putLock(TransactionId tid, const ResourceId &rid,
 LockTableResult
 LockTable::deleteLock(TransactionId tid, const ResourceId &rid) {
   LockListPair &lock_list_pair = internal_map_[rid];
+
+  // Each resource id has its own and pending locks list.
   LockOwnList &lock_own_list = lock_list_pair.first;
   LockPendingList &lock_pending_list = lock_list_pair.second;
 
+  // Iterate over owned locks list to see the lock entry of the transaction
+  // on the resource id exists.
   for (LockOwnList::const_iterator it = lock_own_list.begin();
        it != lock_own_list.end(); ++it) {
     if (it->first == tid) {
+      // If it exists, erase it from the owned list.
       lock_own_list.erase(it);
 
+      // Since we erased a lock entry from owned list, the first entries
+      // in the pending list can be pushed to owned list if they are
+      // compatible with the remaining owned entries.
       movePendingToOwned(rid);
 
       return LockTableResult::kDEL_FROM_OWNED;
     }
   }
-  for (LockPendingList::const_iterator it = lock_pending_list.begin();
-       it != lock_pending_list.end();
-       ++it) {
+
+  // Iterate over pending locks list to check the lock entry of the transaction
+  // on this resource id exists.
+  for (LockPendingList::const_iterator it = lock_pending_list.begin(),
+	 end = lock_pending_list.end(); it != end; ++it) {
     if (it->first == tid) {
+      // If it exists, erase it from pending list.
       lock_pending_list.erase(it);
       return LockTableResult::kDEL_FROM_PENDING;
     }
   }
+
+  // Execution reaches here, if we cannot find the corresponding lock entry
+  // in the both list.
   return LockTableResult::kDEL_ERROR;
 }
 
@@ -75,31 +100,42 @@ void LockTable::movePendingToOwned(const ResourceId &rid) {
   LockOwnList &lock_own_list = lock_list_pair.first;
   LockPendingList &lock_pending_list = lock_list_pair.second;
 
-  for (LockPendingList::const_iterator pending_it = lock_pending_list.begin();
-       pending_it != lock_pending_list.end(); ++pending_it) {
+  // Iterate over pending list to pending requests compatible with the all entries
+  // in the resource ids owned lock list.
+  for (LockPendingList::const_iterator pending_it = lock_pending_list.begin(),
+	 end = lock_pending_list.end(); pending_it != end; ++pending_it) {
     TransactionId pending_tid = pending_it->first;
     AccessMode pending_mode = pending_it->second.getAccessMode();
     bool is_compatible_with_own_list = true;
 
-    for (LockOwnList::const_iterator owned_it = lock_own_list.begin();
-         owned_it != lock_own_list.end();
-         ++owned_it) {
+    // Now compare against the all entries in the owned lock list.
+    for (LockOwnList::const_iterator owned_it = lock_own_list.begin(),
+	   end = lock_pending_list.end(); owned_it != end; ++owned_it) {
       AccessMode owned_mode = owned_it->second.getAccessMode();
 
       if (!pending_mode.isCompatible(owned_mode)) {
-        is_compatible_with_own_list = false;
-        break;
+	// If it is not compatible, we will not move this entry.
+	is_compatible_with_own_list = false;
+	break;
       }
     }
+
+    // If this pending lock entry is compatible with the all entries in the
+    // owned lock list, we should move it from pending list to owned list.
     if (is_compatible_with_own_list) {
-      // TODO(Hakan) Check this if logic. List erase invalidates the
-      //             iterator, therefore we reassign pending_it.
+      // Erase the entry from the list. Get the new iterator.
       pending_it = lock_pending_list.erase(pending_it);
+
+      // Put the corresponding entry to the own list.
       lock_own_list.emplace_back(pending_tid, Lock(rid, pending_mode));
+
+      // Move iterator one step backward because erasing an element moves the
+      // iterator to the next element.
       --pending_it;
     } else {
-      // No need for iterating pending list more since
-      // we find first incompatible one.
+      // There is no need to iterate pending list anymore since
+      // we found first incompatible one. Checking, and accepting other pending
+      // entries may cause starvation.
       break;
     }
   }
